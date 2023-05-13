927b7feca9b0ee8ec037d42f7a41e89b88156ff2
Swapnil Tanpure <70815048+swapnil-tanpure23@users.noreply.github.com>
2023-05-13 16:52:47 +0530

    Update Email.java

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -11,47 +11,29 @@ public class Email {
         this.password = "Accio@123";
     }
 
-    public String getEmailId() {
-        return emailId;
+    public void changePassword(String oldPassword, String newPassword) {
+    if (oldPassword.equals(password) && isValidPassword(newPassword)) {
+        password = newPassword;
     }
+}
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void changePassword(String oldPassword, String newPassword){
-        //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
-        // 1. It contains at least 8 characters
-        // 2. It contains at least one uppercase letter
-        // 3. It contains at least one lowercase letter
-        // 4. It contains at least one digit
-        // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
-        
-        if (!this.password.equals(oldPassword)) {
+private boolean isValidPassword(String password) {
+    if (password.length() < 8) {
         return false;
     }
-    if (newPassword.length() < 8) {
-        return false;
-    }
-    boolean hasUpperCase = false;
-    boolean hasLowerCase = false;
-    boolean hasDigit = false;
-    boolean hasSpecialChar = false;
-    for (char ch : newPassword.toCharArray()) {
-        if (Character.isUpperCase(ch)) {
+    boolean hasUpperCase = false, hasLowerCase = false, hasDigit = false, hasSpecial = false;
+    for (char c : password.toCharArray()) {
+        if (Character.isUpperCase(c)) {
             hasUpperCase = true;
-        } else if (Character.isLowerCase(ch)) {
+        } else if (Character.isLowerCase(c)) {
             hasLowerCase = true;
-        } else if (Character.isDigit(ch)) {
+        } else if (Character.isDigit(c)) {
             hasDigit = true;
         } else {
-            hasSpecialChar = true;
+            hasSpecial = true;
         }
     }
-    if (!hasUpperCase || !hasLowerCase || !hasDigit || !hasSpecialChar) {
-        return false;
-    }
-    this.password = newPassword;
-    return true;
+    return hasUpperCase && hasLowerCase && hasDigit && hasSpecial;
+}
 }
 }
