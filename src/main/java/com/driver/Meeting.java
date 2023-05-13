eb390179ea32370efee1405ab557f2523413f172
Swapnil Tanpure <70815048+swapnil-tanpure23@users.noreply.github.com>
2023-05-13 17:11:21 +0530

    Update Meeting.java

--- src/main/java/com/driver/Meeting.java
+++ src/main/java/com/driver/Meeting.java
@@ -10,12 +10,34 @@ public class Meeting {
         this.startTime = startTime;
         this.endTime = endTime;
     }
-     public LocalTime getStartTime() {
+      public LocalTime getStartTime() {
         return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
     }
 
     public LocalTime getEndTime() {
         return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public Duration getDuration() {
+        return Duration.between(startTime, endTime);
+    }
+
+    public boolean overlapsWith(Meeting other) {
+        if (this.getStartTime().isBefore(other.getStartTime()) && this.getEndTime().isAfter(other.getStartTime())) {
+            return true;
+        }
+        if (this.getStartTime().isAfter(other.getStartTime()) && this.getStartTime().isBefore(other.getEndTime())) {
+            return true;
+        }
+        return false;
     }
 
 }
